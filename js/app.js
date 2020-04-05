fdaf68ed790d7a46721d8aa58f55ab166a1dc24a
Josh Cubero <jnatc1@gmail.com>
2020-04-04 21:11:31 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,95 @@
+/**Column One Content*/
+let playerScore = document.getElementById('player-score');
+let playerTally = 0;
+let playerChoice = document.getElementById('player-choice');
+let playerInput = document.getElementById('player-input');
+
+/**Column Two*/
+let columnTwo = document.getElementById('col-2');
+let gameResult = document.createElement('p');
+columnTwo.appendChild(gameResult);
+gameResult.style.fontSize = '50px';
+gameResult.style.color = 'red';
+let submitBtn = document.querySelector('.submit');
+let resetBtn = document.querySelector('.reset');
+let eraseBtn = document.querySelector('.erase');
+
+/**Column Three*/
+let computerScore = document.getElementById('computer-score');
+let computerTally = 0;
+
+let computerChoice = document.getElementById('computer-choice');
+
+//playerChoice.src = "img-"+randoNum+".jpg";
+let computerPlay = () => {
+  let randoNum = Math.floor((Math.random() * 3) + 1);
+  switch (randoNum) {
+    case 1:
+      computerChoice.src = "img-"+randoNum+".jpg";
+      return "rock";
+    case 2:
+      computerChoice.src = "img-"+randoNum+".jpg";
+      return "paper";
+    case 3:
+      computerChoice.src = "img-"+randoNum+".jpg";
+      return "scissors";
+  }
+};
+
+let playerPic = () =>{
+  switch(playerInput.value){
+    case 'rock':
+      playerChoice.src = 'img-1.jpg';
+      break;
+      case 'paper':
+      playerChoice.src = 'img-2.jpg';
+      break;
+      case 'scissors':
+      playerChoice.src = 'img-3.jpg';
+      break;
+  }
+};
+
+submitBtn.addEventListener('click', () => {
+  let computerSelection = computerPlay();
+  playerPic();
+  if (playerInput.value === 'rock' && computerSelection === 'scissors') {
+    gameResult.innerHTML = "Player 1 Wins!";
+    playerTally += 1;
+  } 
+  else if (playerInput.value === 'scissors' && computerSelection === 'paper') {
+    playerTally += 1;
+    gameResult.innerHTML = "Player 1 Wins!";
+  } 
+  else if (playerInput.value === 'paper' && computerSelection === 'rock') {
+    playerTally += 1;
+    gameResult.innerHTML = "Player 1 Wins!";
+  } 
+  else if (playerInput.value === computerSelection) {
+    gameResult.innerHTML = "We Have a Draw!";
+  } 
+  else {
+    computerTally += 1;
+    gameResult.innerHTML = "Player 1 Loses, Computer Wins!";
+  }
+playerScore.innerHTML = playerTally;
+computerScore.innerHTML = computerTally;
+console.log("Computer " + computerSelection);
+console.log("Player " + playerInput.value);
+});
+
+resetBtn.addEventListener('click', () => {
+  gameResult.innerHTML = '';
+  computerScore.innerHTML = 0;
+  playerScore.innerHTML = 0;
+  computerChoice.src = "";
+  playerChoice.src = "";
+  playerInput.value = "";
+});
+
+eraseBtn.addEventListener('click', () => {
+  gameResult.innerHTML = '';
+  computerChoice.src = "";
+  playerChoice.src = "";
+  playerInput.value = "";
+});
